1b64ef7fbe74c7a436a3f322068066ed3f94702b
Sandra <sandra0924@protonmail.com>
2024-12-11 10:01:24 +0100

    renamed features.php to treats.php

---
+++ treats.php
@@ -0,0 +1,133 @@
+<?php require_once(__DIR__ . '/header.php'); ?>
+
+<section class="hero" id="hero-exp">
+    <h1>Treat Your<br />Undead Self</h1>
+    <img src="images/spa2.png" alt="" />
+</section>
+<a href="#explore" class="cta">Explore</a>
+</header>
+<main>
+    <article id="explore">
+        <div class="inner-wrapper grid">
+            <section>
+                <h3>Wellness</h3>
+                <h2>Bone-Breaking Massage</h2>
+                <p>Centuries of sleeping inside coffins is destined to leave a trace on one's body. This bone-breaking massage is a given cure for that rigor mortis. In about an hour, our masseuse is sure to soften up those stiff joints and leave you feeling as splendid as a newborn again.</p>
+                <p><a href="/" class="cta">Book now</a></p>
+            </section>
+            <img src="images/spa6.png" alt="" />
+        </div>
+    </article>
+    <article>
+        <div class="inner-wrapper grid">
+            <section>
+                <h3>Wellness</h3>
+                <h2>Virgin Blood Bath</h2>
+                <p>Ever since the Countess of Hungary set the trend of Virgin Blood Baths back in 1590, vampires all over the world have tried to follow this example. Nowadays, though, virgins of legal age are both hard to come by and even harder to reap. Thus, we are proud to present a version of this beauty regime based upon synthetic blood. After an hour soaked in this hemoglobin-rich substance, one's skin is sure to sparkle even brighter than Twilight.</p>
+                <p><a href="/" class="cta">Book now</a></p>
+            </section>
+            <img src="images/spa1.png" alt="" />
+        </div>
+    </article>
+    <article>
+        <div class="inner-wrapper grid">
+            <section>
+                <h3>Wellness</h3>
+                <h2>Stake Through the Heart</h2>
+                <p>When one is undead yet destined to live forever, it is sometimes hard to remember what it is like to feel alive. Stake Trough the Heart is a unique near true death experience that may make even the most confident vampire fear for their eternal life. Our stakes are made of fine, harmless ivory that are thin enough to leave nothing more than a wound that heals in an instant, but the pain will feel just as real and may in return remind you to be grateful of your undead existence.</p>
+                <p><a href="/" class="cta">Book now</a></p>
+            </section>
+            <img src="images/stake3.jpeg" alt="" />
+        </div>
+    </article>
+    <article id="experiences">
+        <div class="inner-wrapper flex">
+            <section>
+                <h3>Fitness</h3>
+                <h2>Muscle Dungeon</h2>
+                <p>Muscular and fit vampires often hit the gym in order to instill the fascade of keeping up their physique the human way. Going to a regular gym, though, means one has to hold back in order to not wake any suspicion. The Muscle Dungeon, however, have all the equipment a vampire could wish for and allows for a true strength test between real beasts.</p>
+                <p><a href="/" class="cta">Book now</a></p>
+            </section>
+            <img src="images/gym3.jpeg" class="bg" id="gym" alt="" />
+        </div>
+    </article>
+    <article>
+        <div class="inner-wrapper grid">
+            <section>
+                <h3>Mindfulness</h3>
+                <h2>Morningstar Meditation</h2>
+                <p>Every vampire needs to train their focus in order to control their instincts and manage their immortality. Recent research suggests that meditation can alter even the non-existent brain waves of a vampire's mind. We offer classes with Lucifer's favourite offspring, Luci Morningstar. So have a seat or hang yourself upside down from the ceiling, close your eyes and lets focus on here and now.</p>
+                <p><a href="/" class="cta">Book now</a></p>
+            </section>
+            <img src="images/goat-yoga2.png" alt="" />
+        </div>
+    </article>
+    <article>
+        <div class="inner-wrapper grid">
+            <section>
+                <h3>Mindfulness</h3>
+                <h2>Moonlight Yoga</h2>
+                <p>Full moons can be stressful, given the widespread howling and increased risk of werewolf attacks. Unwind both body and mind with Moonlight Yoga. Beyond it's calming effects, it also prepares the body for various scenarios where fight or flight might be necessary.</p>
+                <p><a href="/" class="cta">Book now</a></p>
+            </section>
+            <img src="images/yoga8.jpeg" alt="" />
+        </div>
+    </article>
+    <article>
+        <div class="inner-wrapper grid">
+            <section>
+                <h3>Counseling</h3>
+                <h2>Forgive a Foe</h2>
+                <p>Old quarrels risk to continue forever unless one sorts them out. Our therapists are specially trained to work with conflict management between vampires and other mythical creatures. Perhaps it's time to forgive your maker for creating you against your will, or to let go of the grudge you hold against that werewolf who imprinted on your first-born?</p>
+                <p><a href="/" class="cta">Book now</a></p>
+            </section>
+            <img src="images/therapy2.jpeg" alt="" />
+        </div>
+    </article>
+    <article id="exp">
+        <div class="inner-wrapper flex">
+            <section>
+                <h3>Little Ones</h3>
+                <h2>Activities for Little Ones</h2>
+                <p>Vampire guardians of little ones know the struggles of looking after a baby vamp, and being one is not easy either. We therefore offer activities that aim to amuse little ones as well as give their guardians a well deserved pause.</p>
+                <p><a href="#dragons-lair" class="cta">Read More</a></p>
+            </section>
+            <img src="images/children10.jpeg" class="bg" alt="" />
+        </div>
+    </article>
+    <article id="dragons-lair">
+        <div class="inner-wrapper grid">
+            <section>
+                <h3>Little Ones</h3>
+                <h2>Dragon's Lair</h2>
+                <p>For guardians and little ones that don't wish to be parted completely during their stay, there are nightly meet-ups in the Dragon's Lair. Activities include both classic games, like Seek and Destroy, Show Me Your Teeth and Bite Me, as well as more creative ones, such as painting in the blood of one's enemy and taxidermy of one's favourite pet.</p>
+                <p><a href="/" class="cta">Book now</a></p>
+            </section>
+            <img src="images/children7.jpeg" alt="" />
+        </div>
+    </article>
+    <article>
+        <div class="inner-wrapper grid">
+            <section>
+                <h3>Little Ones</h3>
+                <h2>Baby Vamp Camp</h2>
+                <p>At Baby Vamp Camp, little ones get to explore both the castle and its surroundings, while solving mysteries and riddles, as well as forming friendships that could potentially last a lifetime. The camp activities are held out of sight and members have their own quarters to rest in, ensuring that guardians won't be disturbed at all until check-out.</p>
+                <p><a href="/" class="cta">Book now</a></p>
+            </section>
+            <img src="images/baby-vamp-camp.jpeg" alt="" />
+        </div>
+    </article>
+    <article>
+        <div class="inner-wrapper grid">
+            <section>
+                <h3>Little Ones</h3>
+                <h2>South Tower Meetings</h2>
+                <p>Little ones of a mentally mature age often struggle to come to terms with the dysphoria that inevitably comes with the eternal entrapment inside a child's body. Sharing experiences with others in the same situation can help, making one feel less alone. Meetings are held every evening in the South Tower.</p>
+                <p><a href="/" class="cta">Book now</a></p>
+            </section>
+            <img src="images/children13.jpeg" alt="" />
+        </div>
+    </article>
+</main>
+
+<?php require_once(__DIR__ . '/footer.php'); ?>
