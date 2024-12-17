ca19d51194abdce1f7724d2a5b1f5f6d5a06cae6
Sandra <sandra0924@protonmail.com>
2024-12-17 13:27:04 +0100

    altered footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,8 +1,25 @@
 <footer>
+    <section id="features">
+        <div class="inner-wrapper grid-3">
+            <article>
+                <img src="../assets/images/icons/check-circle.svg" alt="" class="feature-icon-1" />
+                <h3>Bask in the Sun</h3>
+                <p>The possibility to see the sun again makes the Lovelace Hotel a given retreat for any vampire.</p>
+            </article>
+            <article>
+                <img src="../assets/images/icons/heart.svg" alt="" class="feature-icon-2" />
+                <h3>Revitalize</h3>
+                <p>We offer a variety of activities and experiences with focus on overall wellbeing, aiming to make our undead guests feel alive.</p>
+            </article>
+            <article>
+                <img src="../assets/images/icons/drop.svg" alt="" class="feature-icon-3" />
+                <h3>Bloody good bites</h3>
+                <p>Sink your teeth into warm flesh, our blood-bags are trained to be gentle, tasteful and discreet.</p>
+            </article>
+            <img src=" ../assets/images/ada13.jpeg" class="bg" id="feats" alt="" />
+        </div>
+    </section>
     <section>&copy; 2024 The Lovelace Hotel</section>
-    <section></section>
-    <section></section>
-    <section></section>
 </footer>
 <script src="../assets/script.js"></script>
 </body>
