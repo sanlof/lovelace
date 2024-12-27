9ede80d577289e539cf6d327eeb8c06f28b78a06
Sandra <sandra0924@protonmail.com>
2024-12-27 16:50:02 +0100

    cleaned up some of the html, added reviews and altered upcoming events

--- index.php
+++ index.php
@@ -22,13 +22,13 @@
             <section>
                 <h3>Rooms & Suites</h3>
                 <h2>Find Your Sanctuary</h2>
-                <p></p>
+                <p>Stay three nights or more – get twenty percent off with code Jan25.</p>
                 <p><a href="../pages/rooms.php" class="cta">View Rooms</a></p>
             </section>
             <img src="../assets/images/room10.jpeg" class="bg" alt="" />
         </div>
     </article>
-    <article class="basic" id="food">
+    <article class="basic">
         <div class="inner-wrapper grid">
             <section>
                 <h3>Restaurant</h3>
@@ -40,23 +40,67 @@
             <img src="../assets/images/restaurant8.jpeg" alt="" />
         </div>
     </article>
-    <article id="experiences">
+    <article>
         <div class="inner-wrapper flex">
             <section>
                 <h3>Experiences</h3>
                 <h2>Treatments & Activities</h2>
-                <p>Vampires of the modern times know, that even though it might not alter one's already perfect complextion, exercise and other treatments are still important to stay in shape and look one's best. We offer a variety of activities and experiences that aim to fill the soulless inner void.</p>
+                <p>Vampires of the modern times know, that even though it might not alter one's already perfect complextion, exercise and other treatments are still important to feel good. When you book a stay with us, you get free access to a variety of activities and experiences which aim to fill the soulless inner void.</p>
                 <p><a href="../pages/treats.php" class="cta">Explore</a></p>
             </section>
             <img src="../assets/images/spa2.png" class="bg" id="exp" alt="" />
         </div>
     </article>
-    <article class="basic" id="food">
+    <article id="reviews">
+        <div class="inner-wrapper grid-3">
+            <section>
+                <div>
+                    <p>&starf; &starf; &starf; &starf; &star;</p>
+                    <p>The blood-bags in this establishment are delicious. However, one will do best to keep their fairies close at all times.</p>
+                </div>
+                <div>
+                    <img src="https://images4.fanpop.com/image/photos/15800000/eric-northman-eric-northman-15837818-1024-576.jpg" alt="" />
+                    <strong>Eric Northman</strong>
+                </div>
+            </section>
+            <section>
+                <div>
+                    <p>&starf; &starf; &starf; &starf; &starf;</p>
+                    <p>I love the stylish and vintage feel of this place, and the virgin blood baths make my skin glow like nothing else!</p>
+                </div>
+                <div>
+                    <img src="https://images6.fanpop.com/image/polls/1504000/1504630_1440706923085_full.jpg?v=1440707021" alt="" />
+                    <strong>Countess Elizabeth</strong>
+                </div>
+            </section>
+            <section>
+                <div>
+                    <p>&starf; &starf; &starf; &starf; &starf;</p>
+                    <p>The South Tower is my favourite thing. I don't care too much for the sun, but those meetings truly light up my immortal life.</p>
+                </div>
+                <div>
+                    <img src="https://i.pinimg.com/originals/04/32/60/043260901819dbfde5d83cda695c2ba1.jpg" alt="" />
+                    <strong>Claudia</strong>
+                </div>
+            </section>
+        </div>
+    </article>
+    <article class="basic">
         <div class="inner-wrapper grid">
             <section>
-                <h3>Upcoming Event</h3>
+                <h3>January 5th</h3>
+                <h2>Eclipse Orchestra</h2>
+                <p>On January 5th, the Ecplise Orchestra is back to give another one of their supernatural concerts. All hotel guests are welcome to gather in the Great Hall and listen as the music from strings and bows deafens the sounds of church bells.</p>
+            </section>
+            <img src="../assets/images/music2.jpeg" alt="" />
+        </div>
+    </article>
+    <article class="basic">
+        <div class="inner-wrapper grid">
+            <section>
+                <h3>January 31st</h3>
                 <h2>Silver Bullet for My Valentine</h2>
-                <p>Every Friday night, dances are arranged in the ballroom with the Chess-tiled floor. On February the 14th, the annual Valentine's ball is held. This year's theme is forbidden love, inspired by the unlucky romance between a vampire and werewolf.</p>
+                <p>The annual ball is arranged every year in the ballroom with the Chess-tiled floor. Hotel guests booked in luxury suites or standard rooms are welcome to join. This year's theme is forbidden love, inspired by the unlucky romance between a vampire and werewolf.</p>
             </section>
             <img src="../assets/images/dance.jpeg" alt="" />
         </div>
